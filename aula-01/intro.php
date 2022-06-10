010aa3a9667de4dba1561999bd4c3b5f913d72a4
Gustavo Galote <gusgalot3g@gmail.com>
2022-06-10 17:58:01 -0300

    feat: add Curso.php and Professor.php

--- aula-01/intro.php
+++ aula-01/intro.php
@@ -1,13 +1,17 @@
 <?php
 
 include 'Aluno.php';
+include 'Professor.php';
+include 'Curso.php';
+
 
 $a1 = new Aluno(); // instanciando
 $a1->cpf = '12345678909';
 $a1->nome = 'Gustavo';
 
+$cursoPHP = new Curso();
+$cursoPHP->nome = 'Introdução ao PHP';
+$cursoPHP->cargaHoraria = 88;
+$cursoPHP->descricao = 'Aprender o básico e o intermediário de PHP';
 
-$aluno = [
-    'nome' => 'João',
-    'cpf' => '123.456.789.89',
-];
+echo "<h1>Aluno: {$a1->nome}</h1>";
