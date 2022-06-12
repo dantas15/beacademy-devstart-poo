002ddee8339ffb878b2db1d2695b485c2c0c6cd9
Gustavo Galote <gusgalot3g@gmail.com>
2022-06-12 11:20:47 -0300

    feat: autoload

--- aula-06/index.php
+++ aula-06/index.php
@@ -1,13 +1,16 @@
 <?php
 
-include 'classes/Usuario.php';
-include 'classes/Categoria.php';
-include 'classes/Cliente.php';
-include 'classes/Gestor.php';
-include 'classes/Produto.php';
-include 'classes/Config/Usuario.php';
+include 'vendor/autoload.php';
 
-echo 'Funcionou!';
+use Classes\Config\Usuario;
+use Classes\Categoria;
 
+//$us1 = new Classes\Usuario();
+//$us2 = new UsuarioConfig();
 
+$us2 = new Usuario();
 
+$c2 = new Categoria();
+
+var_dump($us2);
+var_dump($c2);
