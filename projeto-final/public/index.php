e85c6282c8aa33494e8437f0715c98e0ef8dbbce
Gustavo Galote <gusgalot3g@gmail.com>
2022-06-13 17:28:25 -0300

    finish oop classes

--- projeto-final/public/index.php
+++ projeto-final/public/index.php
@@ -3,8 +3,33 @@
 include '../vendor/autoload.php';
 
 use App\Controller\IndexController;
+use App\Controller\ProductController;
+use App\Controller\ErrorController;
 
-$c = new IndexController();
-$c->indexAction();
+$url = explode('?', $_SERVER['REQUEST_URI'])[0];
 
-echo 'Olá mundo';
+function route(string $controllerName, string $methodName)
+{
+  return [
+    'controller' => $controllerName,
+    'method' => $methodName,
+  ];
+}
+
+$routes = [
+  '/' => route(IndexController::class, 'indexAction'),
+  '/produtos' => route(ProductController::class, 'listAction'),
+  '/produtos/novo' => route(ProductController::class, 'addAction'),
+];
+
+if (!isset($routes[$url])) {
+  (new ErrorController())->notFoundAction();
+  exit;
+}
+
+$controllerName = $routes[$url]['controller'];
+$methodName = $routes[$url]['method'];
+
+(new $controllerName())->$methodName();
+  
+var_dump($routes[$url]);
